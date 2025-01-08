b607c3547e83000511f4b0778067522058cd9234
sagarshinde511 <amplifierelectronics9@gmail.com>
2025-01-08 12:08:34 +0530

    Update busT.py

--- busT.py
+++ busT.py
@@ -24,23 +24,26 @@ if login_button:
         # 1st Tab: Recharge Card
         with tabs[0]:
             st.subheader("Recharge Card")
-            st.write("Redirecting to Google...")
+            st.write("Redirecting to Recharge Card...")
             st.markdown('<a href="https://busrfrecharge-aqupzmfkxy3xbvpoq7hybm.streamlit.app/" target="_blank">Click here if not redirected</a>', unsafe_allow_html=True)
 
         # 2nd Tab: Check Live Status
         with tabs[1]:
-            st.subheader("Check Live Status")
-            st.write("This tab will display live status information.")
+            st.subheader("Live Bus Status")
+            st.write("Redirecting check bus Live passngers...")
+            st.markdown('<a href="https://livebus-itkuyksb6plr5pbzgxhop7.streamlit.app/" target="_blank">Click here if not redirected</a>', unsafe_allow_html=True)
 
         # 3rd Tab: Check History
         with tabs[2]:
-            st.subheader("Check History")
-            st.write("This tab will display the user's transaction or travel history.")
+            st.subheader("Check Travel History")
+            st.write("Redirecting to Recharge Card...")
+            st.markdown('<a href="https://busrfrecharge-aqupzmfkxy3xbvpoq7hybm.streamlit.app/" target="_blank">Click here if not redirected</a>', unsafe_allow_html=True)
 
         # 4th Tab: Register Passenger
         with tabs[3]:
-            st.subheader("Register Passenger")
-            st.write("This tab will allow the admin to register new passengers.")
+            st.subheader("Regester Card")
+            st.write("Redirecting to Reguster Passnger Card...")
+            st.markdown('<a href="https://regesterpassanger-9iu2puyfxmh9hts5azkwdv.streamlit.app/" target="_blank">Click here if not redirected</a>', unsafe_allow_html=True)
     else:
         st.sidebar.error("Invalid credentials. Please try again.")
 else:
