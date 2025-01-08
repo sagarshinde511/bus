e2a449d0c7d9d242f9a5ba4ff5c5f3bbb8ddab03
sagarshinde511 <amplifierelectronics9@gmail.com>
2025-01-08 11:49:04 +0530

    Update busT.py

--- busT.py
+++ busT.py
@@ -24,9 +24,9 @@ if login_button:
         # Buttons for different actions
         if st.button("Recharge Card"):
             st.subheader("Recharge Card")
-            st.write("Here you can recharge RFID cards. (Functionality not implemented yet.)")
-            st.markdown('<a href="https://feedback-dtsqucs9rfckhzrzu648uq.streamlit.app/" target="_blank">Open Recharge Card Page</a>', unsafe_allow_html=True)
-
+            st.write("Redirecting to Google...")
+            st.markdown('<a href="https://www.google.com" target="_blank">Click here if not redirected</a>', unsafe_allow_html=True)
+    '''
         if st.button("Check Traveled History"):
             st.subheader("Check Traveled History")
             st.write("Here you can check travel history. (Functionality not implemented yet.)")
@@ -37,12 +37,11 @@ if login_button:
             st.write("Here is a list of all users. (Functionality not implemented yet.)")
             st.markdown('<a href="https://livebus-itkuyksb6plr5pbzgxhop7.streamlit.app/" target="_blank">Open Users Page</a>', unsafe_allow_html=True)
 
-        # New button to register a passenger
         if st.button("Register Passenger"):
             st.subheader("Register New Passenger")
             st.write("Here you can register a new passenger. (Functionality not implemented yet.)")
             st.markdown('<a href="https://regesterpassanger-9iu2puyfxmh9hts5azkwdv.streamlit.app/" target="_blank">Open Register Passenger Page</a>', unsafe_allow_html=True)
-
+    '''
     else:
         st.sidebar.error("Invalid username or password. Please try again.")
 else:
